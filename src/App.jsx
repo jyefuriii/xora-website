76cd3dfe3b463f6d65715bd8690023208f79bb2b
jyefuriii <jeffrey.t.fabella@gmail.com>
2025-01-08 23:59:51 +0800

    Hero, Features, header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 //import React from "react";
-import Header from "./sections/Header"
+import Features from "./sections/Features";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-      <main className="overflow-hidden">
-      <Header/>
-      </main>
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
   );
 };
 
